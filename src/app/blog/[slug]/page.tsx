5a1a00ed52fd5bf42404cb68d4bcbee6a321c7a0
AdamChoudary <xmgdkydgdjdb@gmail.com>
2025-07-20 20:33:11 +0500

    latest

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -40,7 +40,11 @@ const blogPost = {
   readTime: "8 min read",
 };
 
-export default function BlogPostDetailPage({ params }: { params: { slug: any } }) {
+export default function BlogPostDetailPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   // In a real app, you'd fetch post data based on params.slug
   const post = blogPost; // Using mock data for now
 
